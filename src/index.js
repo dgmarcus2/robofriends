687f8146bf8b5809b7b1acd407b137684a77fbdc
Dave <davidmarcus10@gmail.com>
2018-08-16 08:49:15 -0500

    Added search-functionality to cards and style.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
-import CardList from './CardList';
+import 'tachyons';
+import App from './App';
 
-ReactDOM.render(<CardList />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
